786ecb3bedac14d02486edc19ae1a7e196ac9e83
RhydamP <ronnypandey7@gmail.com>
2024-08-06 19:01:34 +0530

    Update .gitignore to stop tracking src/env.js

--- src/env.js
+++
@@ -1,6 +0,0 @@
-//const serverBaseUrl = "http://localhost:2000/";
-const serverBaseUrl = "http://3.109.122.29:2000/"
-const  serverApiUrl = serverBaseUrl + "api/"
-export default serverApiUrl;
-
-export const Google_Maps_Api_key = 'AIzaSyCXIQ-DX5goj6urxKOuYPRVWemaKttDb34'
