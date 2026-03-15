3744f52a2e25619b50b3d9281b7577c0bb31ee6a
Tiasha <pratyay.mazumdar02@gmail.com>
2026-03-15 17:43:11 +0530

    version 4

--- src/env.js
+++ src/env.js
@@ -1,4 +1,4 @@
-const serverBaseUrl = "http://localhost:2000/";
+const serverBaseUrl = "https://spintrip.in/api/";
 //const serverBaseUrl = "https://spintripbackend.site/"
 //const serverBaseUrl = "http://171.76.84.58:2000/"
 const  serverApiUrl = serverBaseUrl + "api/"
